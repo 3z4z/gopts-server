9a844062c82ac9ac68e20ec9a52fe34336146ce7
Md. Salman Ezaz <md.salmanezaz@gmail.com>
2025-12-05 18:17:34 +0600

    server created

--- src/server.js
+++ src/server.js
@@ -1,2 +1,22 @@
 const express = require("express");
 const cors = require("cors");
+const { connectDB } = require("./config/db");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const port = process.env.PORT || 3000;
+
+async function startServer() {
+  const collections = await connectDB();
+  app.get("/", async (_, res) => {
+    res.send("Server is running");
+  });
+
+  app.listen(port, () => {
+    console.log(`Server is running from: http://localhost:${port}`);
+  });
+}
+
+startServer().catch(console.dir);
